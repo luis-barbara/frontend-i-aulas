782a14ebb5d008428ee5d39b94b4a1072d1e73bb
aluno <aluno@mac25-2.local>
2025-04-04 21:56:38 +0100

    feat: novas funcionalidades

--- community.js
+++ community.js
@@ -1,15 +1,20 @@
 async function getCards() {
     try {
         const response = await fetch("community.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const data = await response.json();
+
         updateCardsContent(data.cards);
-    }   catch (error) {
-        console.error("Erro ao carregar os dados:", error);
+    } catch (error) {
+        console.error("Loading data error:", error);
     }
 }
 
 function updateCardsContent(cards) {
     const container = document.querySelector('.row');
+    container.innerHTML = "";
 
     cards.forEach(card => {
         const cardContainer = document.createElement('div');
@@ -17,13 +22,27 @@ function updateCardsContent(cards) {
 
         const imgElement = document.createElement('img');
         imgElement.src = card.imageUrl; 
-        imgElement.alt = card.title;    
-        imgElement.style.width = '100%'; 
+        imgElement.alt = `${card.title} - ${card.description}`;   
+        imgElement.loading = "lazy";
+
+        const tagElement = document.createElement('span');
+        tagElement.className = 'tag';
+        tagElement.textContent = card.tag;
 
         cardContainer.appendChild(imgElement); 
-
         container.appendChild(cardContainer);
+        cardContainer.appendChild(tagElement);
     });
 }
 
-getCards();
+window.onload = () => {
+    getCards();
+};
+
+
+
+
+// localStorage.setItem("key", "value");
+
+
+// sessionStorage.setItem("key", "value");
