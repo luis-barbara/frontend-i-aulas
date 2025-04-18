99bf1182cf8b0deaa797ce2af2fd919dea546348
luis-barbara <luis.carlos.barbara@gmail.com>
2025-04-18 18:26:18 +0100

    feat: ajustes no posicionamento layout

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// hamburger menu and sidebar functionality
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const sidebar = document.querySelector(".sidebar");
 const closeButton = document.querySelector(".sidebar .close-btn");
@@ -24,3 +26,19 @@ document.addEventListener("click", (event) => {
         hamburgerMenu.classList.remove("change");
     }
 });
+
+
+// Dynamic text
+const text = "What should we create today?";
+    const dynamicText = document.getElementById("dynamic-text");
+    let index = 0;
+
+    function typeEffect() {
+        if (index < text.length) {
+            dynamicText.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 60); 
+        }
+    }
+
+    typeEffect();
